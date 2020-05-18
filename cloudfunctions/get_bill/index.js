14b9b1d104c17d8ec662121ab4a928f60cc45dca
minya <1049468118@qq.com>
2020-05-18 20:18:29 +0800

    云函数获取所有账单数据

--- cloudfunctions/get_bill/index.js
+++ cloudfunctions/get_bill/index.js
@@ -6,12 +6,38 @@ cloud.init({
 
 const db = cloud.database();
 
-exports.main = async (event, context) => {
-    return await db.collection('bill')
+//获取数据总数
+async function getCount(event){
+    let count = await db.collection('bill')
+        .where({
+            openid: event.userInfo.openId
+        })
+        .count();
+    return count;
+}
+
+//分段获取数据
+async function getList(event, skip){
+    let list = await db.collection('bill')
         .orderBy('date.timestamp', 'desc')
         .where({
             openid: event.userInfo.openId
         })
+        .skip(skip)
         .get()
+    return list.data;
+}
+
+exports.main = async (event, context) => {
+    let count = await getCount(event);
+    count = count.total;
+    let list = [];
+    for (let index = 0; index < count; index+=100) {
+        list = list.concat(await getList(event, index));
+    }
+    return {
+        data: list,
+        code: 0
+    };
 }
 
